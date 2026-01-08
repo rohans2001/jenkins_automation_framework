49ee86a8185b1e53f0f959a67eafa6cdac599fd7
rohans2001 <rohansamanta2001@gmail.com>
2026-01-08 16:30:33 +0530

    testng

--- src/test/java/com/rohansamanta/jenkinsautomation/FirstTest.java
+++ src/test/java/com/rohansamanta/jenkinsautomation/FirstTest.java
@@ -2,25 +2,36 @@ package com.rohansamanta.jenkinsautomation;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class FirstTest {
 
-    public static void main(String[] args) {
+    WebDriver driver;
 
-        WebDriver driver = new ChromeDriver();
-               driver.manage().window().maximize();
-               driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-               driver.get("https://rohans2001.github.io/");
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://rohans2001.github.io/");
+    }
 
-        String title = driver.getTitle();
-        if("Rohan Samanta".equals(title)){
-            System.out.println("Test Passed");
-        }else{
-            System.out.println("Test Failed");
-        }
+    @Test
+    public void verifyWebsiteTitle() {
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Rohan Samanta";
+
+        Assert.assertEquals(actualTitle, expectedTitle, "Title does not match!");
+    }
+
+    @AfterMethod
+    public void tearDown() {
         driver.quit();
-
     }
 }
+
