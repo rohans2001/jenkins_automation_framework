ddcc74cac42397903bf51a9fbb9a0573a78427c1
rohans2001 <rohansamanta2001@gmail.com>
2026-01-08 16:56:07 +0530

    allure report addition

--- src/test/java/com/rohansamanta/jenkinsautomation/FirstTest.java
+++ src/test/java/com/rohansamanta/jenkinsautomation/FirstTest.java
@@ -1,5 +1,8 @@
 package com.rohansamanta.jenkinsautomation;
 
+import io.qameta.allure.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -22,14 +25,25 @@ public class FirstTest {
     }
 
     @Test
+    @Description("Verify website title is correct")
+    @Severity(SeverityLevel.CRITICAL)
     public void verifyWebsiteTitle() {
-        String actualTitle = driver.getTitle();
+        String actualTitle = getWebsiteTitle();
         String expectedTitle = "Rohan Samanta";
 
         Assert.assertEquals(actualTitle, expectedTitle, "Title does not match!");
     }
 
+    @Step("Getting website title")
+    public String getWebsiteTitle() {
+        return driver.getTitle();
+    }
+
     @AfterMethod
+    @Attachment(value = "Screenshot on failure", type = "image/png")
+    public byte[] captureScreenshot() {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
     public void tearDown() {
         driver.quit();
     }
